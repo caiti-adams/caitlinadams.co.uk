0d239125e312ee7afe96b46872f90ef8cd3a8d9e
Max Adams <max.adams@kingmakers.com>
2026-01-15 19:27:31 +0000

    Astro install

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,38 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_iSpwSU3c.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/about.astro.mjs');
+const _page1 = () => import('./pages/posts/_slug_.astro.mjs');
+const _page2 = () => import('./pages/posts.astro.mjs');
+const _page3 = () => import('./pages/projects.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/about.astro", _page0],
+    ["src/pages/posts/[slug].astro", _page1],
+    ["src/pages/posts.astro", _page2],
+    ["src/pages/projects.astro", _page3],
+    ["src/pages/index.astro", _page4]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./noop-entrypoint.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "b7b290fc-1b5d-4855-a6ee-3a54e51e9961"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import edgeone from '@edgeone/astro';
+import netlify from '@astrojs/netlify/static';
 
-// https://astro.build/config
 export default defineConfig({
-  /*
-  Currently it is deployed on Tencent Cloud EdgeOne, on an Asia-based Pages platform (the author is also based in Asia). 
-  If users require, they can switch to Vercel or another Pages platform themselves.
-  */
-  adapter: edgeone(),
-  integrations: [tailwind()],
-  server: {
-    port: 4321,
-    host: true
-  },
-  devToolbar: {
-    enabled: false
-  },
-  vite: {
-    server: {
-      headers: {
-        'Cache-Control': 'no-cache, no-store, must-revalidate'
-      }
-    }
-  }
+  adapter: netlify(),
 });
